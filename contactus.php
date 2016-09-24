731d36b4955ff9f141285cce0557682558d3c421
Harshit Kedia <harshit.kedia2015@vit.ac.in>
2016-09-24 23:54:23 +0530

    contactus done

--- contactus.php
+++ contactus.php
@@ -55,55 +55,68 @@ include_once('includes/head.php');
 
 		</div>
 	</section>
-	<section class="margin-top-100">
-	<div class="grid-container">
-		<div class="grid-row">
-			<div class="column-8">
-				<h4>Leave us a message...</h4>
-				<form action="http://flagstone.vidalthemes.com/post.php" class="form">
-					<div class="grid-container">
-						<div class="grid-row margin-bottom-20">
-							<div class="column-6">
-								<input type="text" required class="input margin-bottom-10" placeholder="Name">
+	<section class="margin-top-40">
+		<div class="grid-container">
+			<div class="grid-row">
+				<div class="column-8">
+					<h4>Leave us a message...</h4>
+					<form action="http://flagstone.vidalthemes.com/post.php" class="form">
+						<div class="grid-container">
+							<div class="grid-row margin-bottom-20">
+								<div class="column-6">
+									<input type="text" required class="input margin-bottom-10" placeholder="Name">
+								</div>
+								<div class="column-6">
+									<input type="email" required class="input margin-bottom-10" placeholder="Email">
+								</div>
 							</div>
-							<div class="column-6">
-								<input type="email" required class="input margin-bottom-10" placeholder="Email">
+							<div class="grid-row">
+								<div class="column-12">
+									<textarea class="input margin-bottom-10" name="name" rows="8" cols="40" placeholder="Message"></textarea>
+									<button class="button text-color-10 margin-top-10 margin-bottom-30">Submit</button>
+								</div>
 							</div>
 						</div>
-						<div class="grid-row">
-							<div class="column-12">
-								<textarea class="input margin-bottom-10" name="name" rows="8" cols="40" placeholder="Message"></textarea>
-								<button class="button text-color-10 margin-top-10 margin-bottom-30">Submit</button>
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
-			<div class="column-3 push-column-1 background-color-11 padding-top-20 padding-bottom-20">
-				<h4>Find us...</h4>
-				<ul class="list list--address margin-bottom-20">
-					<li class="list__item list--address-location">Pravega Racing</li>
-					<li class="list__item">VIT University</li>
-					<li class="list__item">Vellore</li>
-					<li class="list__item">India</li>
-					<li class="list__item list--address-phone">+(44)8976 422355</li>
-					<li class="list__item list--address-phone">+(44)8976 422355</li>
-				</ul>
-				<ul class="list">
-					<li class="list__item"><a class="link" href="mailto:email@emailaddress.com">pravegaracing@gmail.com</a></li>
-				</ul>
+					</form>
+				</div>
+				<div class="column-3 push-column-1 background-color-11 padding-top-20 padding-bottom-20">
+					<h4>Find us...</h4>
+					<ul class="list list--address margin-bottom-20">
+						<li class="list__item list--address-location"><b>Pravega Racing</b></li>
+						<li class="list__item">VIT University</li>
+						<li class="list__item">Vellore</li>
+						<li class="list__item">India</li>
+						<li class="list__item list--address-phone">+(44)8976 422355</li>
+						<li class="list__item list--address-phone">+(44)8976 422355</li>
+					</ul>
+					<ul class="list">
+						<li class="list__item"><a class="link" href="mailto:email@emailaddress.com">pravegaracing@gmail.com</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
+	<section class="text-center">
+		<div style='overflow:hidden;height:440px;width:100vw;margin-left:auto;margin-right: auto;'>
+		<div id='gmap_canvas' style='height:440px;width:100vw;'>
+		</div>
+		<div>
+			<small>
+				<a href="http://www.embedgooglemaps.com/en/">Generate your map here, quick and easy!									Give your customers directions Get found</a></small>
+			</div><div>
+			<small><a href="https://www.amazon.com/Extra-Large-Folding-Kennel-Plastic/dp/B00M3NBJ8E/ref=sr_1_5?s=pet-supplies&ie=UTF8&qid=1470313482&sr=1-5keywords=pet+cage">Pet Wire Cage</a></small></div>
+			<style>
+				#gmap_canvas img{max-width:none!important;background:none!important}
+			</style>
+		</div>
+		<script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script>
+		<script type='text/javascript'>function init_map(){var myOptions = {zoom:11,center:new google.maps.LatLng(12.9723913,79.15767519999997),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(12.9723913,79.15767519999997)});infowindow = new google.maps.InfoWindow({content:'<strong>Pravega Racing</strong><br>VIT University, Vellore, India<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);
+		</script>
+	</section>
 
-
-
-
-
-<a href="#" class="back-to-top--floating"><i class="ion-ios-arrow-up"></i></a>
-<?php
-include_once("includes/footer.php");
-?>
+	<a href="#" class="back-to-top--floating"><i class="ion-ios-arrow-up"></i></a>
+	<?php
+	include_once("includes/footer.php");
+	?>
 </body>
 </html>
